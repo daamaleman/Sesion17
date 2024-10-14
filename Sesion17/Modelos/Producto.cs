395157f90e1ebacfa4b9380110fbbb4ed2d8f8f6
daamaleman <diedereicha@uamv.edu.ni>
2024-10-14 07:21:04 -0600

    Agregué la clase Producto

---
+++ Sesion17/Modelos/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion17.Modelos
+{
+    // Creacion de clase
+    public struct Producto
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+    }
+}
